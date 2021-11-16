98d907752f0126ba9975ef09ee64dbd27962bad9
somasekhar1 <somasekhar0023@gmail.com>
2021-11-16 13:01:53 +0530

    first commi

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'm%c30v(4=6q!_ouzf5xx9l7n89pl0#l16lo6+z*g4!49-63@t#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['loveschat.herokuapp.com']
 
